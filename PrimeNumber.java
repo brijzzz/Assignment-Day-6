53bcbce512f4180d8c0c0b2afc832e8ca7e19b05
BrijalPatel <brijalpatel59@gmail.com>
2022-05-03 17:44:30 +0530

    Added:Logical Program

---
+++ PrimeNumber.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.LogicalProgram;
+
+public class PrimeNumber {
+    static void prime(int N)
+    {
+        int x, y, flg;
+
+        System.out.println(
+                "All the Prime numbers within 1 and " + N
+                        + " are:");
+
+        for (x = 1; x <= N; x++) {
+
+            if (x == 1 || x == 0)
+                continue;
+
+            flg = 1;
+
+            for (y = 2; y <= x / 2; ++y) {
+                if (x % y == 0) {
+                    flg = 0;
+                    break;
+                }
+            }
+
+            if (flg == 1)
+                System.out.print(x + " ");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int N = 75;
+
+        prime(N);
+    }
+}
---
+++ ReverseNumber.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.LogicalProgram;
+
+import java.io.*;
+
+public class ReverseNumber {
+    static int reverse(int n){
+
+        int rev = 0;
+        int rem;
+
+        while(n>0){
+
+            rem = n%10;
+            rev = (rev*10) + rem;
+            n = n/10;
+        }
+
+        return rev;
+    }
+
+    public static void main (String[] args) {
+        int n = 231191;
+
+        System.out.print("Reversed Number: "+ reverse(n));
+    }
+}
